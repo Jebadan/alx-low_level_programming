2d641cadbd043d2b8811ec8f6f223912984bceb6
Jebadan <Josiahebadan@gmail.com>
2023-07-17 00:20:15 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,20 +10,18 @@ int main(void)
 
 	for (i = 0; i < 99; i++)
 	{
-		for (j = 0; j < 100; j++)
+		for (j = i + 1; j < 100; j++)
 		{
-			if (i < j)
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 98 || j != 99)
 			{
-				putchar((i / 10) + 48);
-				putchar((i / 10) + 48);
+
+				putchar(',');
 				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j / 10) + 48);
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
 		}
 	}

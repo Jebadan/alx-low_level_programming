272584bf680e795fa220dded6c91e6fb68f59266
Jebadan <Josiahebadan@gmail.com>
2023-07-19 00:00:56 -0700

    5-sign

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,12 +8,16 @@
  *
  * Return: 1 for letters. 0 for the rest.
  */
-int _isalpha(int c);
+int _isalpha(int c);/*local variable*/
 {
 
-if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 {
 return (1);
 }
+else
+{
 return (0);
+}
+_putchar('\n');
 }

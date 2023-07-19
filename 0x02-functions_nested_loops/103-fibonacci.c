1fba461ba264cfaff08a1ed9e21e2b2f28409138
Jebadan <Josiahebadan@gmail.com>
2023-07-19 04:20:14 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 
-	/**
-	 * main - Entry point
-	 * Description: finds and prints the sum of the even-valued terms,
-	 * followed by a new line
-	 * Return: Always 0 (Success)
-	 */
+/**
+ * main - Entry point
+ * Description: finds and prints the sum of the even-valued terms,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 	int main(void)
 {
 	int i;
